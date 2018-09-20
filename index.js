99895c44c8ec6819891c928215d8593307719afc
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-09-20 12:44:39 -0600

    changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('Hello World');
+
+// Create ten variables about yourself
+var name = Sandy
+Tall
+Brown Skinned
+Eighteen years old
+Badminton Player
+Filipino
+Male
+
+
+
+//Print variables to console
+console.log(name)
