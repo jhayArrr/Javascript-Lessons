63b8eb19de0b91083baade4d889baf9d18c06cf6
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-09-20 13:20:52 -0600

    changes

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 console.log('Hello World');
 
 // Create ten variables about yourself
-var name = Sandy
-Tall
-Brown Skinned
-Eighteen years old
-Badminton Player
-Filipino
-Male
-
-
+let first name = "Sandy";
+let Last name = "Julian";
+let Height = "5'8";
+let Skin Color = "Brown";
+let Age = "18";
+let Nationality = "Filipino";
+let Gender = "Male";
+const ageAfraid = 29; //security feature
+let Canadian Citizen = false;
+let achievements; //undefined, null; originally "none"
+//debug with typeof
 
 //Print variables to console
-console.log(name)
+console.log(first name);
+console.log(Last name);
+console.log(Height);
+console.log(Skin Color);
+console.log(Age);
+console.log(Nationality);
+console.log(Gender);
+console.log(ageAfraid);
+console.log("Am I Canadian Citizen" + male)
