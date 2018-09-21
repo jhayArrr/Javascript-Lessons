30b94ff173335dc7fcda8b5ee861bb6d1d941c0b
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-09-21 13:31:20 -0600

    changes

---
+++ Swapping VAriables Object Start/index.js
@@ -0,0 +1,40 @@
+console.log('Hello World');
+
+// Create ten variables about yourself
+let SandyJulian = {
+  first name: "Sandy"
+  Last name: "Julian"
+  Height: "5'8"
+  Skin Color: "Brown"
+  Age: "18"
+  Nationality: "Filipino"
+  Gender: "Male"
+  ageAfraid: 29
+  Canadian Citizen: false
+  achievements: null
+}
+
+let MeganSpence = {
+  first naem: "Megan",
+  Last name: "Spence",
+  age: 14,
+  ageAfraid: null,
+  hair: true,
+  male: false,
+  female: true,
+}
+
+//printing out entire object
+console.log(SandyJulian)
+
+
+//Print variables to console
+console.log(first name);
+console.log(Last name);
+console.log(Height);
+console.log(Skin Color);
+console.log(Age);
+console.log(Nationality);
+console.log(Gender);
+console.log(ageAfraid);
+console.log("Am I Canadian Citizen" + Not Canadian Citizen)
