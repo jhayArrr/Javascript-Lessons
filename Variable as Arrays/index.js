50ccee3a050b5eb08dfb7faf127f08a6268dc34d
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-09-24 13:54:20 -0600

    changes

--- Variable as Arrays/index.js
+++ Variable as Arrays/index.js
@@ -3,12 +3,12 @@ let SandyJulian = [];
 let MeganSpence = [];
 
 //firstName, lastName, age, ageAfraid, hair, male, female
-SandyJulian= ["sandy", "Julian", "13", "29", "null", "true", "false",];
+SandyJulian = ["sandy", "Julian", "13", "29", "null", "true", "false"];
 MeganSpence = ["Megan", "Spence", "14", "null", "true", "false", "true"];
 
 //printing out entire object
-console.log(SandyJulian[]);
-console.log(MeganSpence[]);
+console.log(SandyJulian);
+console.log(MeganSpence);
 
 //print first names only
 console.log("Last name is: " + Sandy Julian[1]);
