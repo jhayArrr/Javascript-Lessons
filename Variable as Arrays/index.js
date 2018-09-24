527b231ce5bec757bf990f5b37ed190a81b224dd
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-09-24 13:08:52 -0600

    changes

---
+++ Variable as Arrays/index.js
@@ -0,0 +1,15 @@
+//create ten variables about yourself
+let SandyJulian = [];
+let MeganSpence = [];
+
+//firstName, lastName, age, ageAfraid, hair, male, female
+SandyJulian= ["sandy", "Julian", "13", "29", "null", "true", "false",];
+MeganSpence = ["Megan", "Spence", "14", "null", "true", "false", "true"];
+
+//printing out entire object
+console.log(SandyJulian[]);
+console.log(MeganSpence[]);
+
+//print first names only
+console.log("Last name is: " + Sandy Julian[1]);
+console.log("Last name is: " + MeganSpence[1]);
